407a4a8ff118c1a85ca287f720d88ae0fd36053a
Robert <rcoffie22@yahoo.com>
2020-05-29 16:56:08 +0000

    same

--- hubapp/views.py
+++ hubapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
@@ -13,5 +13,11 @@ def index(request):
 
 
 def register(request):
-  context = {}
-  return render(request,'/registration/register.html',context)
+  form = UserCreationForm()
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('/')
+  context = {'form':form,}
+  return render(request,'registration/register.html',context)
