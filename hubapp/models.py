035e07d4cf53e0488549b33bea50b859063d3ef3
Robert <rcoffie22@yahoo.com>
2020-05-30 15:39:56 +0000

    seriously

--- hubapp/models.py
+++ hubapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+class Add(models.Model):
+  Category=(
+    ('Mobile Phone','Mobile Phone'),
+    ('Audio','Audio'),
+  )
+  
+  location=(
+    ('kumasi','kumasi'),
+    ('Accra','Accra'),
+    ('Tamale','Tamale'),
+  )
+  Condition=(
+    ('Used','used'),
+    ('New'),('New')
+  )
+  title = models.CharField(max_length=150)
+  price = models.DecimalField(max_digits=6,decimal_places=2)
+  brand = models.CharField(max_length=50)
+  description = models.TextField()
+  negotiable  = models.BooleanField(default=False)
+
+  
+  
+  def __str__(self):
+    return self.title()
+  
+  
+  
+  
