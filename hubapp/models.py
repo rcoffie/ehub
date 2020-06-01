ddcda32d11429a9bd5fd524b481ea59c2848c223
Robert <rcoffie22@yahoo.com>
2020-06-01 16:03:01 +0000

    monday

--- hubapp/models.py
+++ hubapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Add(models.Model):
   Category=(
     ('Mobile Phone','Mobile Phone'),
-    ('Audio','Audio'),
+    ('Audio/MP3','Audio/MP3'),
   )
   
   location=(
@@ -15,14 +15,22 @@ class Add(models.Model):
   )
   Condition=(
     ('Used','used'),
-    ('New'),('New')
+    ('New','New'),
   )
   title = models.CharField(max_length=150)
   price = models.DecimalField(max_digits=6,decimal_places=2)
   brand = models.CharField(max_length=50)
+  location = models.CharField(max_length=200, null=True,choices=location)
+  category = models.CharField(max_length=200, null=True,choices=Category)
+  condition = models.CharField(max_length=200, null=True,choices=Condition)
   description = models.TextField()
   negotiable  = models.BooleanField(default=False)
-  image = models.ImageField(null=True)
+  default_image = models.ImageField(null=True)
+  image_1 = models.ImageField(null=True)
+  image_2 = models.ImageField(null=True)
+  image_3 = models.ImageField(null=True,blank=True)
+  image_4 = models.ImageField(null=True,blank=True)
+  aproval = models.BooleanField(default=False)
   
   def __str__(self):
     return self.title
