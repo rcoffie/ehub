61137d88d5377700713d115cd00013ff8d169544
Robert <rcoffie22@yahoo.com>
2020-06-06 09:15:03 +0000

    paapa

--- hubapp/models.py
+++ hubapp/models.py
@@ -6,44 +6,21 @@ import datetime
 
 
 # Create your models here.
+
 class Add(models.Model):
-  Category=(
-    ('Mobile Phone','Mobile Phone'),
-    ('Audio/MP3','Audio/MP3'),
+  category=(
+  ('Mobile Phone','Mobile Phone'),
+  ('Audio/MP3','Audio/MP3')
   )
   
   location=(
     ('kumasi','kumasi'),
     ('Accra','Accra'),
-    ('Tamale','Tamale'),
-  )
-  Condition=(
-    ('Used','used'),
-    ('New','New'),
-  )
+    ('Tamale','Tamale')
+    
+    )
   title = models.CharField(max_length=150)
-  price = models.DecimalField(max_digits=6,decimal_places=2)
-  location = models.CharField(max_length=200, null=True,choices=location)
-  category = models.CharField(max_length=200, null=True,choices=Category)
-  condition = models.CharField(max_length=200, null=True,choices=Condition)
-  description = models.TextField()
-  negotiable  = models.BooleanField(default=False,null=True)
-  default_image = models.ImageField(null=True)
-  image_1 = models.ImageField(null=True)
-  image_2 = models.ImageField(null=True)
-  image_3 = models.ImageField(null=True,blank=True)
-  image_4 = models.ImageField(null=True,blank=True)
-  aproval = models.BooleanField(default=False,null=True)
-  user = models.ForeignKey(User, on_delete=models.CASCADE,default=User.id)
-  
-  
-  def __str__(self):
-    return self.title
-
-  
-  
-  
-  
-  
+  price = models.DecimalField(max_digits=6, decimal_places=2)
+  location = models.CharField(max_length=200,choices=location)
   
   
