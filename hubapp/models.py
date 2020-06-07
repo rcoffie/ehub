c28cc5bb7e0fd1b1453ee811e34c8fe702002a88
Robert <rcoffie22@yahoo.com>
2020-06-07 08:22:47 +0000

    sosa

--- hubapp/models.py
+++ hubapp/models.py
@@ -1,26 +1,17 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
-import datetime 
+
 
 
 
 # Create your models here.
 
 class Add(models.Model):
-  category=(
-  ('Mobile Phone','Mobile Phone'),
-  ('Audio/MP3','Audio/MP3')
-  )
-  
-  location=(
-    ('kumasi','kumasi'),
-    ('Accra','Accra'),
-    ('Tamale','Tamale')
-    
-    )
-  title = models.CharField(max_length=150)
-  price = models.DecimalField(max_digits=6, decimal_places=2)
-  location = models.CharField(max_length=200,choices=location)
-  
-  
+  title = models.CharField(max_length=50,)
+  price = models.DecimalField(max_digits=6, decimal_places=2,default=True)
+  used = models.BooleanField(default=False,)
+  default_image = models.ImageField(null=True,)
+  aproval = models.BooleanField(default=False,null=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE,default=User.id)
+  add_date = models.DateTimeField(auto_now_add=True,null=True)
